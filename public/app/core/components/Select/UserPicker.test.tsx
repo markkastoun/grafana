95c469aa87f1da2f9de6f63781b938e40f47948b
Josh Hunt <joshhunt@users.noreply.github.com>
2022-01-31 13:52:50 +1100

    Suppress error messages from UserPicker test

--- public/app/core/components/Select/UserPicker.test.tsx
+++ public/app/core/components/Select/UserPicker.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import { UserPicker } from './UserPicker';
 
 jest.mock('@grafana/runtime', () => ({
@@ -7,8 +7,11 @@ jest.mock('@grafana/runtime', () => ({
 }));
 
 describe('UserPicker', () => {
-  it('renders correctly', () => {
-    render(<UserPicker onSelected={() => {}} />);
+  it('renders correctly', async () => {
+    await act(async () => {
+      render(<UserPicker onSelected={() => {}} />);
+    });
+
     expect(screen.getByTestId('userPicker')).toBeInTheDocument();
   });
 });
